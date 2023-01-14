1b0fcde1ce2af7f75aa88e4ea4174f3d19c17067
sascha radojkovic <scha13@gmx.de>
2023-01-14 08:32:44 +0100

    delete one return

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -26,5 +26,5 @@ export default async function handler(request, response) {
     return response.status(200).json({ status: "Product deleted" });
   }
 
-  return response.status(405).json({ status: "Method not allowed" });
+  response.status(405).json({ status: "Method not allowed" });
 }
